2fee6629fc6d4f54d55a030868a859e8ab7ed916
archedemallo <compta@archedemallo.fr>
2026-02-22 15:44:53 +0100

    Update formulaires-arche-mallo.js

--- formulaires-arche-mallo.js
+++ formulaires-arche-mallo.js
@@ -268,7 +268,12 @@ function resetForm() {
 // UTILITAIRES
 // ============================================================
 function getTodayISO() {
-    return new Date().toISOString().split('T')[0];
+    const d = new Date();
+    return [
+        d.getFullYear(),
+        String(d.getMonth() + 1).padStart(2, '0'),
+        String(d.getDate()).padStart(2, '0')
+    ].join('-');
 }
 
 // ============================================================
