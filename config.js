27aeafa048d096ed06a4db0017e14378191f9c4f
archedemallo <compta@archedemallo.fr>
2026-07-05 14:23:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,9 @@
 // CONFIGURATION GLOBALE — L'Arche de Mallo
 // Modifier uniquement ici pour changer l'URL Apps Script
 // ============================================================
+//https://script.google.com/macros/s/AKfycbzrE8Airs5j5e9X9wOtq6641GEa7VJ8iYubBOSBBjab9_74W6KfQP5iFGD67YmnfRVspg/exec'
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzrE8Airs5j5e9X9wOtq6641GEa7VJ8iYubBOSBBjab9_74W6KfQP5iFGD67YmnfRVspg/exec';
-
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwwibQbDq2iircsT1ZAh5YM9-SgenjlI089QX_Xl2gchXqczOESA5CVnhljUkVfijRpfg/exec';
+                         
 // URL du tableau de bord (même Apps Script, sans paramètre)
 const DASHBOARD_URL = APPS_SCRIPT_URL;
