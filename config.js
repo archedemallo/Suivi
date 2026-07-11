551d94f3a9a7d59853ad763e20884be1f8daca87
archedemallo <compta@archedemallo.fr>
2026-07-11 09:03:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Modifier uniquement ici pour changer l'URL Apps Script
 // ============================================================
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxzbDDZqck1w6-eHBU2Wm74Gyu9lqtXWxNP-oogkJ8XYK1e-HlfqzUIV6RIh7AET6DZQw/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxY3uc9SWtkJAiy5pVV8pkh5PUREDaa7q2Qa8Jq_VUpU0vvmf4DBBoduyg0fk4WkxXOfA/exec';
                          
 // URL du tableau de bord (même Apps Script, sans paramètre)
 const DASHBOARD_URL = APPS_SCRIPT_URL;
