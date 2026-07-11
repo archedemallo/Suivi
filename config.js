bd90e60a28c20c970adb9a38e2b2bce62e520cc0
archedemallo <compta@archedemallo.fr>
2026-07-11 07:45:03 +0200

    MAJ Photo zoom et Icad config.js

--- config.js
+++ config.js
@@ -2,9 +2,8 @@
 // CONFIGURATION GLOBALE — L'Arche de Mallo
 // Modifier uniquement ici pour changer l'URL Apps Script
 // ============================================================
-//https://script.google.com/macros/s/AKfycbzrE8Airs5j5e9X9wOtq6641GEa7VJ8iYubBOSBBjab9_74W6KfQP5iFGD67YmnfRVspg/exec'
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwMcVklMArpToNdLGIXM-a2AViHBbQhvlXgMePHNjfW5u10fct1kIRbCGxW0J2mf_LwUA/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx0XMFOM3LT5NMUecFTkMJVN5aTtxrCNu-cJbsvDfvdzNNRR5pdIX4_uJINIphBeAq9SQ/exec';
                          
 // URL du tableau de bord (même Apps Script, sans paramètre)
 const DASHBOARD_URL = APPS_SCRIPT_URL;
