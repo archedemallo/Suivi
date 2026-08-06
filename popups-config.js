2c0585831735c521228371e677e0496dc6569a73
archedemallo <compta@archedemallo.fr>
2026-08-06 12:53:44 +0200

    Update popups-config.js

--- popups-config.js
+++ popups-config.js
@@ -20,5 +20,5 @@ const MAJ = {
   dureeJours: 7,                // nombre de jours d'affichage à partir de dateDebut
   titre: "Mise à jour",
   // Texte libre. Utiliser <br> pour un retour à la ligne.
-  texte: "Une mise à jour a été effectuée.<br>Décrivez ici les changements apportés."
+  texte: "Une mise à jour a été effectuée.<br>Possibilité d'enregistré un don anonyme et création des reçus fiscaux. Il ne faut donc plus utiliser les reçus manuels"
 };
